6f6e085ba9a44e41763cf813b05a32cec8757e4e
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-23 11:04:15 -0700

    chore: adjust dark threshold

--- utils/random_generator.py
+++ utils/random_generator.py
@@ -2,7 +2,7 @@ from settings import *
 
 
 def color_generator():
-    darkness_threshold = choice([50, 100, 150, 200])
+    darkness_threshold = choice([100, 125,150, 175])
     while True:
         r = randint(0, 255)
         g = randint(0, 255)
