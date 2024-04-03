c75c79ed19834c610a9bf05c5d528255618a4cd1
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-03 10:11:33 -0600

    chore: use SHORTS_SAVE_PATH variable from settings

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os, csv
 from sys import argv, exit
 from utils.opening import create_opening
 from utils.video import video_process, clean_video_folder
-from settings import VIDEO_ASSETS, SHORTS_SAVE_PATH
+from settings import VIDEO_ASSETS
 
 
 def read_csv(openings):
     with open("./contents_idea - shorts.csv") as f:
         reader = csv.reader(f)
-        for row in list(reader)[1:4]:
+        for row in list(reader)[1:5]:
             filename = row[0]
             title = row[1]
             content = row[2]
@@ -34,7 +34,7 @@ def main():
         create_opening(openings)
     elif action == "video":
         print("Processing video")
-        video_process(SHORTS_SAVE_PATH)
+        video_process()
     elif action == "clean":
         print("Cleaning the folder")
         clean_video_folder()
